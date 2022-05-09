2ae1e7c1fc8450e0e28073bfddfadcc6747a1fe2
Denis Popov <popovdw94@gmail.com>
2022-05-09 11:41:31 +0300

    feat: add uploaded file name display

--- react-components/src/components/forms/ImageUpload/ImageUpload.tsx
+++ react-components/src/components/forms/ImageUpload/ImageUpload.tsx
@@ -1,19 +1,28 @@
+import { useState } from 'react';
 import { FormInputProps } from '../../../utils/types/types';
 
-const ImageUpload = ({ data, register, error, clearErrors }: FormInputProps) => (
-  <div>
-    <label className="form_field image-label">
-      {data.label}
-      <input
-        className="form_image"
-        type="file"
-        accept="image/*"
-        {...register(data.name, { ...data.register })}
-        onInput={clearErrors}
-      />
-    </label>
-    <span className="form_error">{error}</span>
-  </div>
-);
+const ImageUpload = ({ data, register, error, clearErrors }: FormInputProps) => {
+  const [file, setFile] = useState('');
+  const handleChange = (files: FileList | null) => {
+    files ? setFile(files[0].name) : setFile('');
+  };
+  return (
+    <div>
+      <label className="form_field image-label">
+        {data.label}
+        <input
+          className="form_image"
+          type="file"
+          accept="image/*"
+          {...register(data.name, { ...data.register })}
+          onInput={clearErrors}
+          onChange={({ target }) => handleChange(target.files)}
+        />
+      </label>
+      <span className="upload-file">{file}</span>
+      <span className="form_error">{error}</span>
+    </div>
+  );
+};
 
 export default ImageUpload;
