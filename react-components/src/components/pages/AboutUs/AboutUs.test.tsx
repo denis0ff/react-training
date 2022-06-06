8d1098aaa0cde7659a4bcee6516824db56bd924d
Denis Popov <popovdw94@gmail.com>
2022-06-06 22:07:46 +0300

    docs: update readme.md, fix: fix about me content

--- react-components/src/components/pages/AboutUs/AboutUs.test.tsx
+++ react-components/src/components/pages/AboutUs/AboutUs.test.tsx
@@ -5,6 +5,6 @@ describe('About us page', () => {
   it('renders about us page', () => {
     render(<AboutUs />);
     expect(screen.getByText(/About us/i)).toBeInTheDocument();
-    expect(screen.getByText(/We are strongly motivated/i)).toBeInTheDocument();
+    expect(screen.getByText(/strongly motivated/i)).toBeInTheDocument();
   });
 });
--- react-components/src/components/pages/AboutUs/AboutUs.tsx
+++ react-components/src/components/pages/AboutUs/AboutUs.tsx
@@ -5,7 +5,7 @@ class AboutUs extends Component {
   render = () => (
     <section>
       <h2>About us</h2>
-      <h3>We are strongly motivated and ready to work hard.</h3>
+      <h3>I am strongly motivated and ready to work hard.</h3>
       <a className="github-link" href="https://github.com/denis0ff">
         My GitHub
       </a>
